099eab925a05e0ada4a5a1aaeed029365b31eb00
vagnerdavid <vagnerdavid00@gmail.com>
2021-04-11 10:33:39 +0200

    main activity elsö része

--- app/src/main/java/com/example/poloprojekt/MainActivity.java
+++ app/src/main/java/com/example/poloprojekt/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.poloprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText text1 = findViewById(R.id.text1);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                i.putExtra("atad", text1.getText().toString());
+                startActivity(i);
+            }
+        });
     }
 }
